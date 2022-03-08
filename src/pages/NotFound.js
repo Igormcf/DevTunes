63ebff1319b695a9677cea1276f4229d1d615e28
Igormcf <igormaciel82@gmail.com>
2022-03-08 14:58:41 -0300

    Fix error in requirement 01

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -3,7 +3,9 @@ import React from 'react';
 class NotFound extends React.Component {
   render() {
     return (
-      <div data-testid="page-not-found" />
+      <divdiv data-testid="page-not-found">
+        <p> Not found </p>
+      </divdiv>
     );
   }
 }
